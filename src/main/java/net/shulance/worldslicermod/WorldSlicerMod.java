f59161c6352c04d0a1b564303a3cd90e31959893
shulance123 <shulance1@gmail.com>
2022-11-27 19:17:21 -0500

    Initial commit

---
+++ src/main/java/net/shulance/worldslicermod/WorldSlicerMod.java
@@ -0,0 +1,15 @@
+package net.shulance.worldslicermod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorldSlicerMod implements ModInitializer {
+	public static final String MOD_ID = "worldslicermod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/shulance/worldslicermod/WorldSlicerModClient.java
@@ -0,0 +1,10 @@
+package net.shulance.worldslicermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WorldSlicerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
